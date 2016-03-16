0e4569e2ed612bd1b85be4a1e18e95db8b6426c3
haocong <haocong.xu@gmail.com>
2016-03-16 17:38:42 +0800

    first-commit

---
+++ example.js
@@ -0,0 +1,6 @@
+var gpio = require('./lib/gpiou3p.js');
+
+gpio.setup(gpio.PIN.GPIO204, gpio.DIRECTION.OUT); // export pin204
+gpio.write(gpio.PIN.GPIO204, 1);                  // write 1 to pin204
+gpio.read(gpio.PIN.GPIO204);                      // read value of pin204
+gpio.unset(gpio.PIN.GPIO204);                     // unexport pin204
